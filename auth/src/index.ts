e721df9bb4224bdfc8145219b111fd219df203c8
Juan Feris <juan.feris@gorillalogic.com>
2021-05-04 15:09:01 -0500

    Adding Async errors support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import 'express-async-errors'
+
 import { NotFoundError } from './errors'
 import { errorHandler } from './middlewares'
 import {
@@ -17,7 +19,7 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*', () => {
+app.all('*', async () => {
 	throw new NotFoundError()
 })
 
