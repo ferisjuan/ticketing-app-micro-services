e05e78cf871ede38c58024d07f6f619172237b4e
Juan Feris <juan.feris@gorillalogic.com>
2021-05-05 17:31:17 -0500

    Using the k8s JWT secret

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -40,6 +40,8 @@ app.use(errorHandler)
 
 const start = async () => {
 	try {
+		if (!process.env.JWT_KEY) throw new Error('JWT must be defined')
+
 		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
