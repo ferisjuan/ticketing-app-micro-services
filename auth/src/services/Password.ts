d7741a15ed22546cb2e3ca44ae38c1f132d2968b
Juan Feris <juan.feris@gorillalogic.com>
2021-05-05 09:51:38 -0500

    Password Hash

---
+++ auth/src/services/Password.ts
@@ -0,0 +1,24 @@
+import { randomBytes, scrypt } from 'crypto'
+import { promisify } from 'util'
+
+const scryptAcync = promisify(scrypt)
+
+export class Password {
+	static async toHash(password: string) {
+		const salt = randomBytes(8).toString('hex')
+		const buf = await buffer(password, salt)
+
+		return `${buf.toString('hex')}.${salt}`
+	}
+
+	static async compare(storedPassword: string, suppliedPassword: string) {
+		const [hashedPassword, salt] = storedPassword.split('.')
+		const buf = await buffer(suppliedPassword, salt)
+
+		return buf.toString('hex') === hashedPassword
+	}
+}
+
+async function buffer(password: string, salt: string) {
+	return (await scryptAcync(password, salt, 64)) as Buffer
+}
