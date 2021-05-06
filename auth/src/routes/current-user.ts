9515d710719e39b0293a2cd9e171f2625584cb45
Juan Feris <juan.feris@gorillalogic.com>
2021-05-06 18:06:03 -0500

    Current User and Auth0 Middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
-import { BadRequestError } from '../errors'
+import { currentUser } from '../middlewares/current-user'
+import { requireAuth } from '../middlewares/require-auth'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-	if (!req.session?.jwt) return res.send({ currentUser: null })
-
-	try {
-		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-
-		res.send({ currentUser: payload })
-	} catch (error) {
-		res.send({ currentUserRouter: null })
+router.get(
+	'/api/users/currentuser',
+	currentUser,
+	requireAuth,
+	(req: Request, res: Response) => {
+		res.send({ currentUser: req.currentUser || null })
 	}
-})
+)
 
 export { router as currentUserRouter }
