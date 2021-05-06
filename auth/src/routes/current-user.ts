a8a65bee35a1f8108e6bba39df829d6beea2d8f7
Juan Feris <juan.feris@gorillalogic.com>
2021-05-06 17:08:11 -0500

    Returning Current User

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { BadRequestError } from '../errors'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-	res.send('Hi Juan!')
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+	if (!req.session?.jwt) return res.send({ currentUser: null })
+
+	try {
+		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+
+		res.send({ currentUser: payload })
+	} catch (error) {
+		res.send({ currentUserRouter: null })
+	}
 })
 
 export { router as currentUserRouter }
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
 import { ValidateRequest } from '../middlewares'
+import { BadRequestError } from '../errors'
+
+import { User } from '../models'
+import { PasswordManager } from '../services'
 
 const router = express.Router()
 
@@ -11,7 +17,33 @@ router.post(
 		body('password').trim().notEmpty().withMessage('Invalid email or password'),
 	],
 	ValidateRequest,
-	async (req: Request, res: Response) => {}
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body
+
+		const existingUser = await User.findOne({ email })
+
+		if (!existingUser) throw new BadRequestError('Invalid credentials')
+
+		const passwordsMatch = await PasswordManager.compare(
+			existingUser.password,
+			password
+		)
+
+		if (!passwordsMatch) throw new BadRequestError('Invalid credentials')
+
+		const userJwt = jwt.sign(
+			{
+				id: existingUser.id,
+				email: existingUser.email,
+			},
+			process.env.JWT_KEY!
+		)
+
+		// Store it on session
+		req.session = { jwt: userJwt }
+
+		res.status(200).send(existingUser)
+	}
 )
 
 export { router as signinRouter }
