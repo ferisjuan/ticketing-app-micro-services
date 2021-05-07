7fdd018c7a1c04872ef6f14bd4e8c76908b2a4a8
Juan Feris <juan.feris@gorillalogic.com>
2021-05-07 09:47:54 -0500

    Signup Test

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('Should await a 201 on successful signup', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password',
+		})
+		.expect(201)
+})
+
+it('Should await a 400 on invalid email', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'asdfesdfg',
+			password: 'password',
+		})
+		.expect(400)
+})
+
+it('Should await a 400 on invalid password', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'p',
+		})
+		.expect(400)
+})
+
+it('Should await a 400 on with missing email and password', async () => {
+	await request(app).post('/api/users/signup').send({}).expect(400)
+})
+
+it('Should await a 400 on with missing email or password', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({ email: 'test@test.com' })
+		.expect(400)
+
+	await request(app)
+		.post('/api/users/signup')
+		.send({ password: 'password' })
+		.expect(400)
+})
