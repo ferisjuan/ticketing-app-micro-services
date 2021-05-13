3e07ad4cb5c8d1f0d84d98edd90155226e03eb43
Juan Feris <juan.feris@gorillalogic.com>
2021-05-13 18:09:53 -0500

    Email Form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,45 @@
-export default () => {
+import { useState } from 'react'
+
+import axios from 'axios'
+
+const Signup = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const onSubmit = async event => {
+		event.preventDefault()
+
+		const response = await axios.post('/api/users/signup', {
+			email,
+			password,
+		})
+
+		console.log(response.data)
+	}
+
 	return (
-		<form>
+		<form onSubmit={onSubmit}>
 			<h1>Sign Up</h1>
 			<div className='form-group'>
 				<label>Email Address</label>
-				<input className='form-control' />
+				<input
+					className='form-control'
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+				/>
 			</div>
 			<div className='form-group'>
 				<label>Password</label>
-				<input className='form-control' type='password' />
+				<input
+					className='form-control'
+					type='password'
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+				/>
 			</div>
-			<buton className='btn btn-primary'>Sign Up</buton>
+			<button className='btn btn-primary'>Sign Up</button>
 		</form>
 	)
 }
+
+export default Signup
