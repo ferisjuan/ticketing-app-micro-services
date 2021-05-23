c045a3c94b78b7e76230151fce3765fadbe3bc7f
Juan Feris <juan.feris@gorillalogic.com>
2021-05-23 09:36:19 -0500

    [auth] Updating MONGO_URI to use a ENV variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,9 @@ import { app } from './app'
 const start = async () => {
 	try {
 		if (!process.env.JWT_KEY) throw new Error('JWT must be defined')
+		if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined')
 
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true,
